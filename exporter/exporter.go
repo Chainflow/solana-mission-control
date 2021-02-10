2e09dff3a96e5aed3d70b257db7992ac6e4a215e
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-02-10 16:32:33 +0530

    Added exporter

---
+++ exporter/exporter.go
@@ -0,0 +1,133 @@
+package exporter
+
+import (
+	// "context"
+	"strconv"
+	"time"
+
+	// "github.com/certusone/solana_exporter/pkg/rpc"
+	"github.com/prometheus/client_golang/prometheus"
+	// "k8s.io/klog/v2"
+
+	"github.com/PrathyushaLakkireddy/solana-prometheus/config"
+	"github.com/PrathyushaLakkireddy/solana-prometheus/monitor"
+)
+
+const (
+	httpTimeout = 5 * time.Second
+)
+
+// var (
+// 	rpcAddr = cfg.
+// 	addr    = flag.String("addr", ":8080", "Listen address")
+// )
+
+// func init() {
+// 	klog.InitFlags(nil)
+// }
+
+type solanaCollector struct {
+	config                  *config.Config
+	totalValidatorsDesc     *prometheus.Desc
+	validatorActivatedStake *prometheus.Desc
+	validatorLastVote       *prometheus.Desc
+	validatorRootSlot       *prometheus.Desc
+	validatorDelinquent     *prometheus.Desc
+	solanaVersion           *prometheus.Desc
+	accountBalance          *prometheus.Desc
+	// nodeHealth              *prometheus.Metric
+}
+
+func NewSolanaCollector(cfg *config.Config) *solanaCollector {
+	return &solanaCollector{
+		config: cfg,
+		totalValidatorsDesc: prometheus.NewDesc(
+			"solana_active_validators",
+			"Total number of active validators by state",
+			[]string{"state"}, nil),
+		validatorActivatedStake: prometheus.NewDesc(
+			"solana_validator_activated_stake",
+			"Activated stake per validator",
+			[]string{"pubkey", "nodekey"}, nil),
+		validatorLastVote: prometheus.NewDesc(
+			"solana_validator_last_vote",
+			"Last voted slot per validator",
+			[]string{"pubkey", "nodekey"}, nil),
+		validatorRootSlot: prometheus.NewDesc(
+			"solana_validator_root_slot",
+			"Root slot per validator",
+			[]string{"pubkey", "nodekey"}, nil),
+		validatorDelinquent: prometheus.NewDesc(
+			"solana_validator_delinquent",
+			"Whether a validator is delinquent",
+			[]string{"pubkey", "nodekey"}, nil),
+		solanaVersion: prometheus.NewDesc(
+			"solana_node_version",
+			"Node version of solana",
+			[]string{"version"}, nil),
+		accountBalance: prometheus.NewDesc(
+			"solana_account_balance",
+			"Account balance",
+			[]string{"acc_balance"}, nil),
+	}
+}
+
+func (c *solanaCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.solanaVersion
+	ch <- c.accountBalance
+}
+
+// func (c *solanaCollector) mustEmitMetrics(ch chan<- prometheus.Metric, response *rpc.GetVoteAccountsResponse) {
+// 	ch <- prometheus.MustNewConstMetric(c.totalValidatorsDesc, prometheus.GaugeValue,
+// 		float64(len(response.Result.Delinquent)), "delinquent")
+// 	ch <- prometheus.MustNewConstMetric(c.totalValidatorsDesc, prometheus.GaugeValue,
+// 		float64(len(response.Result.Current)), "current")
+
+// 	for _, account := range append(response.Result.Current, response.Result.Delinquent...) {
+// 		ch <- prometheus.MustNewConstMetric(c.validatorActivatedStake, prometheus.GaugeValue,
+// 			float64(account.ActivatedStake), account.VotePubkey, account.NodePubkey)
+// 		ch <- prometheus.MustNewConstMetric(c.validatorLastVote, prometheus.GaugeValue,
+// 			float64(account.LastVote), account.VotePubkey, account.NodePubkey)
+// 		ch <- prometheus.MustNewConstMetric(c.validatorRootSlot, prometheus.GaugeValue,
+// 			float64(account.RootSlot), account.VotePubkey, account.NodePubkey)
+// 	}
+// 	for _, account := range response.Result.Current {
+// 		ch <- prometheus.MustNewConstMetric(c.validatorDelinquent, prometheus.GaugeValue,
+// 			0, account.VotePubkey, account.NodePubkey)
+// 	}
+// 	for _, account := range response.Result.Delinquent {
+// 		ch <- prometheus.MustNewConstMetric(c.validatorDelinquent, prometheus.GaugeValue,
+// 			1, account.VotePubkey, account.NodePubkey)
+// 	}
+// }
+
+func (c *solanaCollector) Collect(ch chan<- prometheus.Metric) {
+	// ctx, cancel := context.WithTimeout(context.Background(), httpTimeout)
+	// defer cancel()
+
+	// accs, err := c.rpcClient.GetVoteAccounts(ctx, rpc.CommitmentRecent)
+	// if err != nil {
+	// ch <- prometheus.NewInvalidMetric(c.totalValidatorsDesc, err)
+	// ch <- prometheus.NewInvalidMetric(c.validatorActivatedStake, err)
+	// ch <- prometheus.NewInvalidMetric(c.validatorLastVote, err)
+	// ch <- prometheus.NewInvalidMetric(c.validatorRootSlot, err)
+	// ch <- prometheus.NewInvalidMetric(c.validatorDelinquent, err)
+	// } else {
+	// 	c.mustEmitMetrics(ch, accs)
+	// }
+
+	version, err := monitor.GetVersion(c.config)
+	if err != nil {
+		ch <- prometheus.NewInvalidMetric(c.solanaVersion, err)
+	} else {
+		ch <- prometheus.MustNewConstMetric(c.solanaVersion, prometheus.GaugeValue, 1, version.Result.SolanaCore)
+	}
+
+	bal, err := monitor.GetBalance(c.config)
+	if err != nil {
+		ch <- prometheus.NewInvalidMetric(c.accountBalance, err)
+	} else {
+		s := strconv.FormatInt(bal.Result.Value, 10)
+		ch <- prometheus.MustNewConstMetric(c.accountBalance, prometheus.GaugeValue, 1, s)
+	}
+}
---
+++ exporter/slots.go
@@ -0,0 +1,236 @@
+package exporter
+
+import (
+	"log"
+	"strings"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	// "k8s.io/klog/v2"
+
+	"github.com/PrathyushaLakkireddy/solana-prometheus/config"
+	"github.com/PrathyushaLakkireddy/solana-prometheus/monitor"
+)
+
+const (
+	slotPacerSchedule = 1 * time.Second
+)
+
+var (
+	totalTransactionsTotal = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "solana_confirmed_transactions_total",
+		Help: "Total number of transactions processed since genesis (max confirmation)",
+	})
+
+	confirmedSlotHeight = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "solana_confirmed_slot_height",
+		Help: "Last confirmed slot height processed by watcher routine (max confirmation)",
+	})
+
+	currentEpochNumber = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "solana_confirmed_epoch_number",
+		Help: "Current epoch (max confirmation)",
+	})
+
+	epochFirstSlot = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "solana_confirmed_epoch_first_slot",
+		Help: "Current epoch's first slot (max confirmation)",
+	})
+
+	epochLastSlot = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "solana_confirmed_epoch_last_slot",
+		Help: "Current epoch's last slot (max confirmation)",
+	})
+
+	nodeHealth = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "solana_node_health",
+		Help: "Current health of the node.",
+	})
+
+	balance = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "Account_balance",
+		Help: "Current balance if your account.",
+	})
+
+	aa = prometheus.NewDesc("node_health", "Current health of the node.", nil, prometheus.Labels{})
+
+	// health = prometheus.NewDesc(prometheus.Desc{
+	// 	prometheus.BuildFQName("node_health","node_health","node_health"),
+	// })
+
+	leaderSlotsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "solana_leader_slots_total",
+			Help: "Number of leader slots per leader, grouped by skip status (max confirmation)",
+		},
+		[]string{"status", "nodekey"})
+)
+
+func init() {
+	prometheus.MustRegister(totalTransactionsTotal)
+	prometheus.MustRegister(confirmedSlotHeight)
+	prometheus.MustRegister(currentEpochNumber)
+	prometheus.MustRegister(epochFirstSlot)
+	prometheus.MustRegister(epochLastSlot)
+	prometheus.MustRegister(leaderSlotsTotal)
+	prometheus.MustRegister(nodeHealth)
+	prometheus.MustRegister(balance)
+}
+
+func (c *solanaCollector) WatchSlots(cfg *config.Config) {
+	var (
+		// Current mapping of relative slot numbers to leader public keys.
+		epochSlots map[int64]string
+		// Current epoch number corresponding to epochSlots.
+		epochNumber int64
+		// Last slot number we generated ticks for.
+		watermark int64
+	)
+
+	ticker := time.NewTicker(slotPacerSchedule)
+
+	for {
+		<-ticker.C
+
+		// Get account balance
+
+		bal, err := monitor.GetBalance(cfg)
+		if err != nil {
+			log.Printf("Error while getting account balance : %v", err)
+			continue
+		}
+
+		balance.Set(float64(bal.Result.Value))
+
+		// Get Node Health
+
+		health, err := monitor.GetNodeHealth(cfg)
+		if err != nil {
+			log.Printf("Error while getting node health info : %v", err)
+			continue
+		}
+		var h float64
+		if health.Error.Message != "" {
+			if strings.EqualFold(health.Error.Message, "Node is unhealthy") {
+				h = 0
+			}
+		} else {
+			if strings.EqualFold(health.Result, "ok") {
+				h = 1
+			}
+		}
+
+		nodeHealth.Set(h)
+
+		// Get current slot height and epoch info
+		// ctx, cancel := context.WithTimeout(context.Background(), httpTimeout)
+		resp, err := monitor.GetEpochInfo(cfg)
+		if err != nil {
+			log.Printf("failed to fetch info info, retrying: %v", err)
+			// cancel()
+			continue
+		}
+		// cancel()
+		info := resp.Result
+
+		// Calculate first and last slot in epoch.
+		firstSlot := info.AbsoluteSlot - info.SlotIndex
+		lastSlot := firstSlot + info.SlotsInEpoch
+
+		// totalTransactionsTotal.Set(float64(info.TransactionCount))
+		confirmedSlotHeight.Set(float64(info.AbsoluteSlot))
+		currentEpochNumber.Set(float64(info.Epoch))
+		epochFirstSlot.Set(float64(firstSlot))
+		epochLastSlot.Set(float64(lastSlot))
+
+		// Check whether we need to fetch a new leader schedule
+		if epochNumber != info.Epoch {
+			log.Printf("new epoch at slot %d: %d (previous: %d)", firstSlot, info.Epoch, epochNumber)
+
+			epochSlots, err := monitor.GetLeaderSlots(firstSlot, cfg)
+			if err != nil {
+				log.Printf("failed to request leader schedule, retrying: %v", err)
+				continue
+			}
+
+			log.Printf("%d leader slots in epoch %d", len(epochSlots), info.Epoch)
+
+			epochNumber = info.Epoch
+			log.Printf("we're still in epoch %d, not fetching leader schedule", info.Epoch)
+
+			// Reset watermark to current offset on new epoch (we do not backfill slots we missed at startup)
+			watermark = info.SlotIndex
+		} else if watermark == info.SlotIndex {
+			log.Printf("slot has not advanced at %d, skipping", info.AbsoluteSlot)
+			continue
+		}
+
+		log.Printf("confirmed slot %d (offset %d, +%d), epoch %d (from slot %d to %d, %d remaining)",
+			info.AbsoluteSlot, info.SlotIndex, info.SlotIndex-watermark, info.Epoch, firstSlot, lastSlot, lastSlot-info.AbsoluteSlot)
+
+		// Get list of confirmed blocks since the last request. This is totally undocumented, but the result won't
+		// contain missed blocks, allowing us to figure out block production success rate.
+		rangeStart := firstSlot + watermark
+		rangeEnd := firstSlot + info.SlotIndex - 1
+
+		cfm, err := monitor.GetConfirmedBlocks(rangeStart, rangeEnd, cfg)
+		if err != nil {
+			log.Printf("failed to request confirmed blocks at %d, retrying: %v", watermark, err)
+			// cancel()
+			continue
+		}
+
+		log.Printf("confirmed blocks: %d -> %d: %v", rangeStart, rangeEnd, cfm)
+
+		// Figure out leaders for each block in range
+		for i := watermark; i < info.SlotIndex; i++ {
+			leader, ok := epochSlots[i]
+			abs := firstSlot + i
+			if !ok {
+				// This cannot happen with a well-behaved node and is a programming error in either Solana or the exporter.
+				log.Printf("slot %d (offset %d) missing from epoch %d leader schedule",
+					abs, i, info.Epoch)
+			}
+
+			// Check if block was included in getConfirmedBlocks output, otherwise, it was skipped.
+			var present bool
+			for _, s := range cfm {
+				if abs == s {
+					present = true
+				}
+			}
+
+			var skipped string
+			var label string
+			if present {
+				skipped = "(valid)"
+				label = "valid"
+			} else {
+				skipped = "(SKIPPED)"
+				label = "skipped"
+			}
+
+			leaderSlotsTotal.With(prometheus.Labels{"status": label, "nodekey": leader}).Add(1)
+			log.Printf("slot %d (offset %d) with leader %s %s", abs, i, leader, skipped)
+		}
+
+		watermark = info.SlotIndex
+	}
+}
+
+// func (c *solanaCollector) fetchLeaderSlots(epochSlot int64) (map[int64]string, error) {
+// 	sch, err := c.rpcClient.GetLeaderSchedule(context.Background(), epochSlot)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("failed to get leader schedule: %w", err)
+// 	}
+
+// 	slots := make(map[int64]string)
+
+// 	for pk, sch := range sch {
+// 		for _, i := range sch {
+// 			slots[int64(i)] = pk
+// 		}
+// 	}
+
+// 	return slots, err
+// }
