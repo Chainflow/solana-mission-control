5e8e2f454db727748503be31a179cfe62efae7f3
NagaTulasi <tulasitulip@gmail.com>
2021-04-12 18:41:12 +0530

    installation script added

--- install_script.sh
+++ install_script.sh
@@ -2,6 +2,42 @@
 
 set -e
 
+
+cd $HOME
+
+echo "------ checking for go, if it's not installed then it will be installed here -----"
+
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+if command_exists go ; then
+    echo "Golang is already installed"
+else
+  echo "------- Install dependencies -------"
+  sudo apt update
+  sudo apt install build-essential jq -y
+
+  wget https://dl.google.com/go/go1.15.5.linux-amd64.tar.gz
+  tar -xvf go1.15.5.linux-amd64.tar.gz
+  sudo mv go /usr/local
+
+  echo "------ Update bashrc ---------------"
+  export GOPATH=$HOME/go
+  export GOROOT=/usr/local/go
+  export GOBIN=$GOPATH/bin
+  export PATH=$PATH:/usr/local/go/bin:$GOBIN
+  echo "" >> ~/.bashrc
+  echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+  echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+  echo 'export GOBIN=$GOPATH/bin' >> ~/.bashrc
+  echo 'export PATH=$PATH:/usr/local/go/bin:$GOBIN' >> ~/.bashrc
+
+  source ~/.bashrc
+
+  mkdir -p "$GOBIN"
+fi
+
 cd $HOME
 
 echo "----------- Installing grafana -----------"
@@ -20,82 +56,102 @@ sudo -S systemctl start grafana-server
 
 cd $HOME
 
-echo "----------- Installing Prometheus -----------"
-
-
-# Make prometheus user
-sudo adduser --no-create-home --disabled-login --shell /bin/false --gecos "Prometheus Monitoring User" prometheus
-
-# Make directories and dummy files necessary for prometheus
-sudo mkdir /etc/prometheus
-sudo mkdir /var/lib/prometheus
-sudo touch /etc/prometheus/prometheus.yml
-sudo touch /etc/prometheus/prometheus.rules.yml
-
-# Assign ownership of the files above to prometheus user
-sudo chown -R prometheus:prometheus /etc/prometheus
-sudo chown prometheus:prometheus /var/lib/prometheus
-
-# Download prometheus and copy utilities to where they should be in the filesystem
+echo "----------- Installing prometheus -----------"
 
 wget https://github.com/prometheus/prometheus/releases/download/v2.22.1/prometheus-2.22.1.linux-amd64.tar.gz
+tar -xvf prometheus-2.22.1.linux-amd64.tar.gz
 
 tar -xvf prometheus-2.22.1.linux-amd64.tar.gz
 
-sudo cp prometheus-2.22.1.linux-amd64/prometheus $GOBIN
+cp prometheus-2.22.1.linux-amd64/prometheus $GOBIN
 
-sudo cp prometheus-2.22.1.linux-amd64/prometheus.yml $HOME
+cp prometheus-2.22.1.linux-amd64/prometheus.yml $HOME
 
 
-# Assign the ownership of the tools above to prometheus user
-sudo chown -R prometheus:prometheus /etc/prometheus/consoles
-sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
-sudo chown prometheus:prometheus /usr/local/bin/prometheus
-sudo chown prometheus:prometheus /usr/local/bin/promtool
+echo "------- Edit prometheus.yml --------------"
 
-# Populate configuration files
-cat ./prometheus/prometheus.yml | sudo tee /etc/prometheus/prometheus.yml
-cat ./prometheus/prometheus.rules.yml | sudo tee /etc/prometheus/prometheus.rules.yml
-cat ./prometheus/prometheus.service | sudo tee /etc/systemd/system/prometheus.service
+echo "
+  - job_name: 'validator'
+    static_configs:
+    - targets: ['localhost:26660']
+  - job_name: 'node_exporter'
+    static_configs:
+    - targets: ['localhost:9100']" >> "$HOME/prometheus.yml"
 
-# systemd
+if [ -z "${SENTRY1}" ];
+then
+   echo "-----Sentry-1 IP is empty, so not writing into prometheus.yml--------"
+else 
+  echo "
+  - job_name: 'sentry-1'
+    static_configs:
+    - targets: ['$SENTRY1:26660']" >> "$HOME/prometheus.yml"
+fi
+
+if [ -z "${SENTRY2}" ];
+then
+  echo "-----Sentry-2 IP is empty, so not writing into prometheus.yml--------"
+else
+  echo "
+  - job_name: 'sentry-2'
+    static_configs:
+    - targets: ['$SENTRY2:26660']" >> "$HOME/prometheus.yml"
+fi
+
+echo "------- Setup prometheus system service -------"
+
+echo "[Unit]
+Description=Prometheus
+After=network-online.target
+[Service]
+Type=simple
+ExecStart=$HOME/go/bin/prometheus --config.file=$HOME/prometheus.yml
+Restart=always
+RestartSec=3
+LimitNOFILE=4096
+[Install]
+WantedBy=multi-user.target" | sudo tee "/lib/systemd/system/prometheus.service"
+
+echo "------ Start prometheus -----------"
+
 sudo systemctl daemon-reload
-sudo systemctl enable prometheus
-sudo systemctl start prometheus
+sudo systemctl enable prometheus.service
+sudo systemctl start prometheus.service
 
-echo "-----------------Installing Node Exporter---------------------"
+echo "-------- Installing node exporter -----------"
 
-# Make node_exporter user
-sudo adduser --no-create-home --disabled-login --shell /bin/false --gecos "Node Exporter User" node_exporter
-
-# Download node_exporter and copy utilities to where they should be in the filesystem
+cd $HOME
 
 curl -LO https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
 
 tar -xvf node_exporter-0.18.1.linux-amd64.tar.gz
 
-sudo cp node_exporter-0.18.1.linux-amd64/node_exporter $GOBIN
+cp node_exporter-0.18.1.linux-amd64/node_exporter $GOBIN
 
-sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+echo "---------- Setup Prometheus Node exporter service -----------"
 
-# systemd
-cat ./node/node_exporter.service | sudo tee /etc/systemd/system/node_exporter.service
+echo "[Unit]
+Description=Node_exporter
+After=network-online.target
+[Service]
+Type=simple
+ExecStart=$HOME/go/bin/node_exporter
+Restart=always
+RestartSec=3
+LimitNOFILE=4096
+[Install]
+WantedBy=multi-user.target" | sudo tee "/lib/systemd/system/node_exporter.service"
+
+echo "----------- Start node exporter ------------"
 
 sudo systemctl daemon-reload
-sudo systemctl enable node_exporter
-sudo systemctl start node_exporter
 
+sudo systemctl enable node_exporter.service
 
-echo "--------- Cloning cosmos-validator-mission-control -----------"
+sudo systemctl start node_exporter.service
 
-cd go/src/github.com
+echo "---- Cleaning .dep .tar.gz files of grafana, prometheus, influxdb and node exporter --------"
 
-git clone https://github.com/PrathyushaLakkireddy/solana-prometheus
+rm influxdb_1.8.3_amd64.deb grafana_7.3.1_amd64.deb node_exporter-0.18.1.linux-amd64.tar.gz prometheus-2.22.1.linux-amd64.tar.gz
 
-cd solana-prometheus
-
-cp example.config.toml config.toml
-
-echo "------ Building and running the code --------"
-
-go build && ./solana-prometheus
+echo "** Done with prerequisite installtion **"
