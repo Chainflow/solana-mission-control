284d17d7b622838de74e93b839278e111bf49f6a
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-07-20 14:24:58 +0530

    Update script

--- scripts/install_script.sh
+++ scripts/install_script.sh
@@ -68,12 +68,14 @@ sudo cp prometheus-2.22.1.linux-amd64/prometheus.yml $HOME
 echo "------- Edit prometheus.yml --------------"
 
 echo "
-
   - job_name: 'solana'
+
     static_configs:
-      - targets: ['localhost:1234']
+    - targets: ['localhost:1234']
+
 
   - job_name: 'node_exporter'
+
     static_configs:
     - targets: ['localhost:9100']" >> sudo "$HOME/prometheus.yml"
 
